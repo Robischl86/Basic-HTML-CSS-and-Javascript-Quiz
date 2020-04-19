b3190eab8ff51a6acd2e95263b505705f04dc370
Levi Robischon <alex@alex-hansen.com>
2020-04-18 23:42:35 -0700

    First commit

---
+++ scripts.js
@@ -0,0 +1,21 @@
+const name = document.getElementById("enter_name")
+
+const begin = document.getElementById("begin");
+
+const restart = document.getElementById("restart");
+
+const correct = document.getElementsByClassName("correct");
+
+
+
+
+
+
+
+function correctAnswer() {
+    points++;
+    return points;
+  }
+
+
+
